596ea748cf3cf67f7bb3812294a472dd62ef5548
shajib <sunhailshajib1@gmail.com>
2022-09-19 09:23:17 +0600

    update file

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { showUserForm, userFormDataMail  } = require('../controller/userController');
 const multer = require('multer');
 const path = require('path');
+const userMulter = require('../middlewares/userMulter');
 
 //init router
 const router = express.Router();
